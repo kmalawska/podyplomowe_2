ed446994447460d6b6aacc0e0490bade24760af2
malawska <mac.kamila@gmail.com>
2019-11-23 16:16:00 +0100

    dodatnie grafiki

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(123);
         System.out.println(456);
+        System.out.println(789);
+
 	// ten program nic ciekawego nie robi
+
     }
 }
